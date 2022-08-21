a87c371f95ec28bfc4bf1e06b105eb8d3be787a0
Rachael Shipes <Rachael@Rachaels-MacBook-Pro-2.local>
2022-08-21 11:43:53 -0400

    completed exercise

---
+++ queries_playstore.sql
@@ -0,0 +1,22 @@
+-- Comments in SQL Start with dash-dash --
+-- 1. playstore=# SELECT * FROM analytics WHERE id = 1880;
+-- 2. playstore=# SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+-- 3. playstore=# SELECT category, COUNT(*) FROM analytics GROUP BY category;
+-- 4. playstore=# SELECT * FROM analytics ORDER BY reviews desc LIMIT 5;
+-- 5. playstore=# SELECT app_name, rating, reviews FROM analytics WHERE rating >= 4.8 ORDER BY reviews desc LIMIT 1;
+-- 6. playstore=# SELECT category, AVG(rating) AS avg FROM analytics GROUP BY category ORDER BY avg desc;
+-- 7. playstore=# SELECT app_name, rating, price FROM analytics WHERE rating < 3 ORDER BY price desc LIMIT 1;
+-- 8. playstore=# SELECT * FROM analytics WHERE min_installs <=50 AND rating IS NOT NULL ORDER BY rating desc;
+-- 9. playstore=# SELECT app_name FROM analytics WHERE reviews >= 10000 AND rating < 3;
+-- 10. playstore=# SELECT app_name, reviews, price FROM analytics WHERE price BETWEEN 0.10 AND 1.00 ORDER BY reviews desc LIMIT 10;
+-- 11. playstore=# SELECT app_name, last_updated FROM analytics ORDER BY last_updated asc LIMIT 1;
+-- 12. playstore=# SELECT app_name, price FROM analytics WHERE price = (SELECT MAX(PRICE) FROM analytics);
+-- 13. playstore=# SELECT SUM(reviews) FROM analytics;
+-- 14. playstore=# SELECT category FROM analytics GROUP BY category HAVING COUNT(*) > 300;
+-- 15. playstore=# SELECT app_name, reviews, min_installs, min_installs/ reviews AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY proportion desc LIMIT 1;
+
+-- fs1. playstore=# SELECT app_name, rating, category FROM analytics WHERE (rating, category) in (SELECT MAX(rating), category FROM analyticsn WHERE min_installs >= 50000 GROUP BY category) ORDER BY category;
+-- fs2. playstore=# SELECT app_name FROM analytics WHERE app_name ILIKE '%facebook%';
+-- fs3. playstore=# SELECT app_name, genres FROM analytics WHERE array_length(genres, 1) = 2;
+-- fs4. playstore=# SELECT app_name, genres FROM analytics WHERE genres @> '{"Education"}';
+
---
+++ queries_products.sql
@@ -0,0 +1,22 @@
+-- Comments in SQL Start with dash-dash --
+-- 1. products_db=# INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44, 'false');
+-- 2. products_db=# INSERT INTO products (name, price, can_be_returned) VALUES ('stool',25.99 , 'true');  
+-- 3. products_db=# INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124, 'false');    
+-- 4. products_db=# SELECT * FROM products;                                                                                                   ;
+--  id | name  | price | can_be_returned 
+-- ----+-------+-------+-----------------
+--   1 | chair |    44 | f
+--   2 | stool | 25.99 | t
+--   3 | table |   124 | f
+-- (3 rows)
+-- 5. products_db=# SELECT name FROM products;
+-- 6. products_db=# SELECT name, price FROM products;
+-- 7. products_db=# INSERT INTO products (name, price, can_be_returned) VALUES ('lamp', 20, 'true');
+-- 8. products_db=# SELECT name FROM products WHERE can_be_returned = 'true';
+-- 9. products_db=# SELECT name FROM products WHERE price < 44;
+-- 10. products_db=# SELECT name FROM products WHERE price BETWEEN 22.50 AND 99.99;
+-- 11. products_db=# UPDATE products SET price = price -19; 
+-- -- decreased by 19 due to lamp price of 20 constriant
+-- 12. products_db=# DELETE FROM products WHERE price < 25;
+-- 13. products_db=# UPDATE products SET price = price +20;
+-- 14. products_db=# UPDATE products SET can_be_returned = 'true';
